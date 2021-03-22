945124654be034fee0575b5193db341205e6d502
winpzs <winpzs@gmail.com>
2021-03-22 16:02:56 +0800

    OPT

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-// import { VueUieditor } from 'vue-uieditor'
+import { VueUieditor } from './vue-uieditor'
 
 Vue.config.productionTip = false
-// Vue.use(VueUieditor)
+Vue.use(VueUieditor)
 
 new Vue({
   router,
