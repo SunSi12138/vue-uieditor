5c765b0dab89ec2fff996fa3078844f725668608
winpzs <winpzs@gmail.com>
2021-03-24 11:04:56 +0800

    ADD: vue-extends

---
+++ src/vue-uieditor/base/vue-extends.ts
@@ -0,0 +1,324 @@
+import Vue, { ComponentOptions, PropOptions, WatchOptions } from "vue";
+import Component from "vue-class-component";
+import { VueClass } from 'vue-class-component/lib/declarations';
+import { Constructor } from "vue-property-decorator";
+
+Component.registerHooks([
+  'beforeRouteEnter',
+  'beforeRouteLeave',
+  'beforeRouteUpdate' // for vue-router 2.2+
+]);
+
+export type UEVueMixin<V extends Vue> = ComponentOptions<Vue> | typeof Vue;
+export interface UEVueComponentOptions<V extends Vue> extends ComponentOptions<V> {
+}
+
+export function UEVueComponent<V extends Vue>(options: UEVueComponentOptions<V> & ThisType<V>): <VC extends VueClass<V>>(target: VC) => VC {
+  return function (target: Function) {
+    const prototype = target.prototype;
+    const mixin = prototype[_getMinxinKey(false)];
+    const mixin_afters = prototype[_getMinxinKey(true)];
+    let mixins = [];
+    if (options.mixins) mixins = mixins.concat(options.mixins);
+    if (mixin) mixins.push(mixin);
+    if (mixin_afters) mixins.push(mixin_afters);
+
+    if (mixins.length > 0)
+      options.mixins = mixins;
+    let componentFactory: any = Component(options);
+    return componentFactory(target);
+  };
+}
+
+function _getMinxinKey(after?: boolean) {
+  return ['_ue_mixins', after === true ? '1' : '0'].join('_');
+}
+
+// function _getMixin<V extends Vue>(target: any, after?: boolean): UEMixin<V>[] {
+//   let key = _getMinxinKey(after);
+//   return target[key] || {};
+// }
+
+function _setMixin<V extends Vue>(target: any, mixin: UEVueMixin<V>, after?: boolean): UEVueMixin<V>[] {
+  const key = _getMinxinKey(after);
+  const newMixin = target[key] = Vue['util'].mergeOptions(target[key] || {}, mixin);
+  return newMixin;
+}
+
+/**
+ * UEProp(String)
+ * @param options 
+ * @returns 
+ */
+export function UEVueProp(options?: (PropOptions | Constructor[] | Constructor)): PropertyDecorator {
+  return function (target: any, propKey: string) {
+    _setMixin(target, {
+      props: {
+        [propKey]: !options ? { type: null } : options
+      }
+    });
+  };
+}
+
+/**
+  * 定义 Inject
+  * @param provide 
+  * @example
+  *  UEVueProvide('ppName')
+  *  private _tstPo() {
+  *      return this.name + 'asdfasf';
+  *  }
+  *
+  */
+export function UEVueProvide(provide?: string): PropertyDecorator {
+  return function (target: any, propKey: string) {
+    _setMixin(target, {
+      provide() {
+        return {
+          [provide || propKey]: target[propKey].call(this)
+        }
+      }
+    });
+  };
+}
+
+
+/**
+ * 注入 Provide
+ * @param provide 
+ * @param defaultValue 
+ * @example
+ *  UEVueInject('ppName')
+    private _ppName:any;
+ */
+export function UEVueInject(provide?: string, defaultValue?: any): PropertyDecorator {
+  return function (target: any, propKey: string) {
+    _setMixin(target, {
+      inject: {
+        [propKey]: { from: provide || propKey, default: defaultValue }
+      }
+    });
+  };
+}
+
+
+/**
+ * vue data
+ * @example
+ *   UEVueData()
+ *   private _initData1() {
+ *       this.name1 = '11111';
+ *       this.name2 = '2222';
+ *      return {name3: '3333'};
+ *   }
+ */
+export function UEVueData(): PropertyDecorator {
+  return function (target: any, propKey: string) {
+    _setMixin(target, {
+      data() {
+        const data = target[propKey].call(this);
+        return data && { ...data } || {};
+      }
+    });
+  };
+}
+
+const fnType = typeof (function () { });
+
+function _getWatchPath(target: any, propKey: string, path: any): string {
+  if (typeof path === fnType) {
+    let key = ['_ue_watch_computed', propKey].join('_');
+    _setMixin(target, {
+      computed: {
+        [key]() {
+          return path.apply(this, arguments);
+        }
+      }
+    })
+    return key;
+  } else {
+    return path;
+  }
+}
+
+/**
+ * 
+ * @param path 
+ * @param options 
+ * @param change 
+ * @returns 
+ * @example
+ * UEVueWatch('name')
+ * private _wName(val){}
+ */
+export function UEVueWatch<T = UEVue>(path: string | ((this: T) => any), options?: WatchOptions, change?: boolean): MethodDecorator {
+  const { deep = false, immediate = false } = options || {};
+  return function (target: any, propKey: string) {
+    const wProp = _getWatchPath(target, propKey, path);
+    _setMixin(target, {
+      watch: {
+        [wProp]: {
+          handler: function (value, old) {
+            target[propKey].apply(this, arguments);
+          }, deep, immediate
+        }
+      }
+    });
+  };
+}
+
+export type UEVueLifeName = 'beforeCreate' | 'created' | 'beforeMount' | 'mounted' |
+  'beforeDestroy' | 'destroyed' | 'beforeUpdate' | 'updated' |
+  'activated' | 'deactivated' | 'beforeRouteEnter' | 'beforeRouteUpdate' | 'beforeRouteLeave';
+
+function _setLife(target: any, lifeName: UEVueLifeName, fn: any, after?: boolean) {
+  _setMixin(target, {
+    [lifeName]: fn
+  }, after);
+}
+
+
+/**
+ * vue data
+ * @example
+ *   UEVueLife('created')
+ *   private _created1() {
+ *   }
+ */
+export function UEVueLife(lifeName: UEVueLifeName, after?: boolean): PropertyDecorator {
+  return function (target: any, propKey: string) {
+    _setLife(target, lifeName, target[propKey], after);
+  };
+}
+
+
+/**
+ * 
+ * @param event 事件名称
+ * @param once 是否只触发一次
+ * @param el 是否绑定到el
+ */
+export function UEVueEvent<T = Vue>(event: string, once?: boolean, el?: ((this: T, el: HTMLElement) => HTMLElement | Document | Window) | boolean): PropertyDecorator;
+/**
+ * 
+ * @param event 事件名称
+ * @param p 选项
+ * @param el 是否绑定到el
+ */
+export function UEVueEvent<T = Vue>(event: string, p?: {
+  /** 是否只触发一次，默认为false */
+  once?: boolean;
+  /** 是否路由activated才触发, 如果有el参数默认为true 否则为 false */
+  activated?: boolean;
+}, el?: ((this: T, el: HTMLElement) => HTMLElement | Document | Window) | boolean): PropertyDecorator;
+export function UEVueEvent<T = Vue>(event: string, p: boolean | {
+  /** 是否只触发一次，默认为false */
+  once?: boolean;
+  /** 是否路由activated才触发, 如果有el参数默认为true 否则为 false */
+  activated?: boolean;
+} = false, el: ((this: T, el: HTMLElement) => HTMLElement | Document | Window) | boolean = false): PropertyDecorator {
+  const isPObj = typeof p === 'object';
+  const once = isPObj ? p['once'] === true : p === true;
+  const activated = isPObj ? p['activated'] === true : !!el;
+  return function (target: any, propKey: string) {
+    const lifeObj: any = {};
+    if (activated) {
+      lifeObj.activated = function () {
+        this._ue_event_activated = true;
+      };
+      lifeObj.deactivated = function () {
+        this._ue_event_activated = false;
+      };
+    }
+    if (!el) {
+      lifeObj.created = function () {
+        this._ue_event_activated = true;
+        let fn = function () {
+          if (activated && !this._ue_event_activated) return;
+          target[propKey].apply(this, arguments);
+        }.bind(this);
+        this[once ? '$once' : '$on'](event, fn);
+      };
+
+    } else {
+      (function () {
+        let isEnd = false;
+        let lastEl;
+        const targetFn = target[propKey];
+        let $this = this;
+        lifeObj.created = function () {
+          this._ue_event_activated = true;
+          $this = this;
+        };
+        let fn = function () {
+          if (isEnd) return;
+          if (once) isEnd = true;
+          if (activated && !this._ue_event_activated) return;
+          if ($this._ue_event_destroyed) return;
+          targetFn.apply($this, arguments);
+        };
+        const upFn = function () {
+          if ($this._ue_event_destroyed) return;
+          let $el = $this.$el;
+          if (typeof el === fnType)
+            $el = el['call']($this, $el);
+          if (lastEl && lastEl != $el) {
+            lastEl.removeEventListener(event, fn);
+          }
+          lastEl = $el;
+          if (!$el) return;
+          $el.addEventListener(event, fn);
+        };
+        lifeObj.updated = function () {
+          if ($this._ue_event_timeId) return;
+          $this._ue_event_timeId = setTimeout(function () {
+            $this._ue_event_timeId = null;
+            upFn();
+          }, 50);
+        };
+        lifeObj.mouated = upFn;
+        lifeObj.destroyed = () => {
+          $this._ue_event_destroyed = true;
+          if (lastEl) {
+            lastEl.removeEventListener(event, fn)
+            lastEl = null;
+          }
+        };
+      })();
+    }
+    _setMixin(target, lifeObj);
+  };
+}
+
+
+export class UEVue extends Vue {
+
+  static getVueName(vnode) {
+    return vnode && vnode.componentOptions ? vnode.componentOptions.Ctor.options.name : '';
+  }
+
+  get $vueName(): string {
+    return UEVue.getVueName(this.$vnode);
+  }
+
+  readonly props: void;
+  readonly store: void;
+
+  readonly beforeCreate: void;
+  readonly created: void;
+  readonly beforeMount: void;
+  readonly mounted: void;
+  readonly beforeDestroy: void;
+  readonly destroyed: void;
+  readonly beforeUpdate: void;
+  readonly updated: void;
+  readonly activated: void;
+  readonly deactivated: void;
+  readonly computed: void;
+
+  readonly beforeRouteEnter: void;
+  readonly beforeRouteUpdate: void;
+  readonly beforeRouteLeave: void;
+
+}
+
--- src/vue-uieditor/vue-uieditor.component.ts
+++ src/vue-uieditor/vue-uieditor.component.ts
@@ -14,10 +14,11 @@ import './layui/lay/modules/slider.js';
 import './layui/lay/modules/tree.js';
 import './layui/lay/modules/selectInput.js';
 import { Layuidestroy, LayuiInit } from './layui-test';
+import { UEVue, UEVueComponent, UEVueLife, UEVueProp } from './base/vue-extends';
 
 declare const layui:any;
 
-@Component({
+@UEVueComponent({
   created(){
     console.warn('created', this);
   },
@@ -28,11 +29,23 @@ declare const layui:any;
     Layuidestroy(this.$el);
   }
 })
-export default class VueUieditor extends Vue {
-  @Prop() private msg!: string;
+export default class VueUieditor extends UEVue {
+  @UEVueProp() private msg!: string;
 
-  created(){
-    console.warn('this', this);
+
+  @UEVueLife('created')
+  private _created1(){
+    console.warn('_created1 1111', this);
   }
+ 
+  @UEVueLife('created')
+  private _created2(){
+    console.warn('_created1 2222', this);
+  } 
+
   
+  @UEVueLife('destroyed')
+  private _destroyed1(){
+    console.warn('destroyed 1111', this);
+  } 
 }
