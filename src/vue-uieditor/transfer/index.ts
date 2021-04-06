01e82ba0a9d2e4946d5289e297f70656fb94a29b
winpzs <winpzs@gmail.com>
2021-04-06 11:31:20 +0800

    ADD: VueTransfer

--- src/vue-uieditor/transfer/index.ts
+++ src/vue-uieditor/transfer/index.ts
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import { UERender } from '../base/ue-render';
 import { BaseTransfer } from "./base-transfer";
+import { VueTransfer } from './vue-transfer';
 
-UERender.AddGlobalTransfer(BaseTransfer);
+UERender.AddGlobalTransfer(BaseTransfer, VueTransfer);
---
+++ src/vue-uieditor/transfer/vue-transfer.ts
@@ -0,0 +1,119 @@
+import { UETransfer } from '../base/ue-base';
+import _ from 'lodash';
+import { UERender } from '../base/ue-render';
+import { UECompiler } from '../base/ue-compiler';
+
+const groupOrder = 1;
+const group = '公用组件库/Vue 组件';
+
+export const VueTransfer: UETransfer = UERender.DefineTransfer({
+  'uieditor-vue-def': {
+    type: 'div',
+    transfer(render, extend) {
+      let content = extend.getPropValue('content', true);
+      if (content) {
+        const babelContent = UECompiler.babelTransform(`function __bg_vue_def_ctx(){ return ${content}; }`);
+        let contentDef = (new Function('__bg_vue_def_', `with(__bg_vue_def_) { return { mixins: [(function() { ${babelContent.code}; return __bg_vue_def_ctx(); })(), { data: function(){ __bg_vue_def_.$this = this; return {}; } }] } }`))({ $this: {}, ...extend.global });
+        extend.extendMixin(contentDef)
+      }
+      // if (extend.editing) return render;
+      return null;
+    },
+    "editor": {
+      group,
+      groupOrder,
+      show: false,
+      text: "vue-def",
+      inline: true,
+      empty: '$$vue-def',
+      attrs: {
+        content: {
+          group: '组件',
+          bind: true,
+          editorBind: true,
+          effect: true,
+          isVueDef: true,
+          value: ''
+        },
+        class: { effect: false }
+      }
+    }
+  },
+  'keep-alive': {
+    transfer(render, extend) {
+      if (extend.editing)
+        render.type = 'div';
+      return render;
+    },
+    "editor": {
+      text: 'keep-alive',
+      order: 0,
+      groupOrder,
+      group,
+      icon: 'layui-icon layui-icon-template-1',
+      base: false,
+      container: true,
+      className: 'drawing-item p-sm',
+      attrs: {
+        'include,exclude,max': {
+          group: '组件',
+          bind: true,
+          value: ''
+        }
+      }
+    }
+  },
+  'router-view': {
+    "editor": {
+      text: 'router-view',
+      order: 0,
+      groupOrder,
+      group,
+      icon: 'layui-icon layui-icon-app',
+      empty: 'router-view'
+    }
+  },
+  'router-link': {
+    transfer(render, extend) {
+      if (extend.editing) {
+        render.type = 'span';
+      }
+      return render;
+    },
+    "editor": {
+      text: 'router-link',
+      order: 0,
+      groupOrder,
+      group,
+      icon: 'layui-icon layui-icon-share',
+      base: false,
+      container: true,
+      className: 'drawing-item p-sm',
+      transferAttr({ render }) {
+        if (_.size(render.children) == 0) {
+          render.children = [{ type: 'uieditor-text', props: { text: 'router-link' } }];
+        }
+      },
+      attrs: {
+        to: {
+          group: '组件',
+          enabledBind: true,
+          value:'/path1'
+        }
+      }
+    }
+  },
+  // 'uieditor-span': {
+  //   type: 'span',
+  //   "editor": {
+  //     text: 'Span 行内标签',
+  //     order: 0,
+  //     groupOrder,
+  //     group,
+  //     icon: 'layui-icon layui-icon-more',
+  //     base: true,
+  //     container: false,
+  //     className: 'drawing-item p-sm'
+  //   }
+  // }
+});
